45b792ce495968c6d86f011deb45c97fdbd11a11
janreynevado <48706387+janreynevado@users.noreply.github.com>
2019-05-08 14:58:30 +0800

    Add files via upload

--- plot4.R
+++ plot4.R
@@ -20,17 +20,16 @@ data$DateTime <- as.POSIXct(DateTime)
 par(mfcol = c(2,2), mar = c(4,4,2,1), oma = c(0,0,2,0))
 with(data, {
         plot(Global_active_power~DateTime, type = "l", 
-             ylab = "Global Active Power (kilowatts)", xlab = "")
+             ylab = "Global Active Power", xlab = "")
         plot(Sub_metering_1~DateTime, type = "l", 
-             ylab = "Global Active Power (kilowatts)", xlab = "")
+             ylab = "Energy sub metering", xlab = "")
         lines(Sub_metering_2~DateTime, col = 'Red')
         lines(Sub_metering_3~DateTime, col = 'Blue')
         legend("topright", col = c("black", "red", "blue"), lty = 1, bty = "n", cex = 0.8,
                legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
         plot(Voltage~DateTime, type = "l", 
-             ylab = "Voltage (volt)", xlab = "")
-        plot(Global_reactive_power~DateTime, type = "l", 
-             ylab = "Global Rective Power (kilowatts)", xlab = "")
+             ylab = "Voltage")
+        plot(Global_reactive_power~DateTime, type = "l")
 })
 dev.copy(png,"plot4.png", width = 480, height = 480)
 dev.off()
